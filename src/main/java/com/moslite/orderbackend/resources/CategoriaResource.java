bc60d5ba44286ddc7093121023b695b771c2433d
Murilo Souza <moslite@hotmail.com>
2021-04-22 06:45:11 -0300

    Criados os repository e service da entidade Categoria

--- src/main/java/com/moslite/orderbackend/resources/CategoriaResource.java
+++ src/main/java/com/moslite/orderbackend/resources/CategoriaResource.java
@@ -1,7 +1,11 @@
 package com.moslite.orderbackend.resources;
 
 import com.moslite.orderbackend.domain.Categoria;
+import com.moslite.orderbackend.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +16,13 @@ import java.util.List;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @GetMapping
-    public List<Categoria> listar() {
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
-        List<Categoria> categorias = new ArrayList<>();
-        categorias.add(cat1);
-        categorias.add(cat2);
-        return categorias;
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria categoria = categoriaService.buscar(id);
+        return ResponseEntity.ok().body(categoria);
     }
 
 }
