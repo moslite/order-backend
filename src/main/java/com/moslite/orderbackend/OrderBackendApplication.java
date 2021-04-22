c5559c6655b75eef390d0d0c87df29db257b82c2
Murilo Souza <moslite@hotmail.com>
2021-04-22 06:50:32 -0300

    Criado o método de inicialização para criar os objetos iniciais do nosso banco de dados

--- src/main/java/com/moslite/orderbackend/OrderBackendApplication.java
+++ src/main/java/com/moslite/orderbackend/OrderBackendApplication.java
@@ -1,13 +1,28 @@
 package com.moslite.orderbackend;
 
+import com.moslite.orderbackend.domain.Categoria;
+import com.moslite.orderbackend.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class OrderBackendApplication {
+public class OrderBackendApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(OrderBackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
