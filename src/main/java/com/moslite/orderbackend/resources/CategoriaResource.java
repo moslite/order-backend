3d963b1097643c618a20fcf393276f1d1387a8f3
Murilo Souza <moslite@hotmail.com>
2021-06-16 00:00:11 -0300

    Paginação com parâmetros adicionais

--- src/main/java/com/moslite/orderbackend/resources/CategoriaResource.java
+++ src/main/java/com/moslite/orderbackend/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import com.moslite.orderbackend.domain.Categoria;
 import com.moslite.orderbackend.dto.CategoriaDTO;
 import com.moslite.orderbackend.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -51,6 +52,18 @@ public class CategoriaResource {
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         categoriaService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction
+    ) {
+        Page<Categoria> list = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDTO = list.map(CategoriaDTO::new);
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
