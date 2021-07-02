25d6d58d27083425f8a86af1fd00500f0ffe9f23
Murilo Souza <moslite@hotmail.com>
2021-07-02 11:47:59 -0300

    Validação customizada de CPF ou CNPJ na inserção do cliente

---
+++ src/main/java/com/moslite/orderbackend/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.moslite.orderbackend.services.validation;
+
+import com.moslite.orderbackend.domain.enums.TipoCliente;
+import com.moslite.orderbackend.dto.ClienteNewDTO;
+import com.moslite.orderbackend.resources.exceptions.FieldMessage;
+import com.moslite.orderbackend.services.validation.utils.BR;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCodigo()) && !BR.isValidCPF(objDto.getCpfCnpj())) {
+            list.add(new FieldMessage("cpfCnpj", "CPF inválido"));
+        }
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfCnpj())) {
+            list.add(new FieldMessage("cpfCnpj", "CNPJ inválido"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
