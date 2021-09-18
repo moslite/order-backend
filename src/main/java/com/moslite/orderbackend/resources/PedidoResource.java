da6a0e43a00ae5199783d318013c7b0fa4954eff
Murilo Souza <moslite@hotmail.com>
2021-09-18 15:30:56 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/moslite/orderbackend/resources/PedidoResource.java
+++ src/main/java/com/moslite/orderbackend/resources/PedidoResource.java
@@ -5,6 +5,7 @@ import com.moslite.orderbackend.domain.Pedido;
 import com.moslite.orderbackend.dto.CategoriaDTO;
 import com.moslite.orderbackend.services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -31,6 +32,17 @@ public class PedidoResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<Pedido>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "instant") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "DESC") String direction
+    ) {
+        Page<Pedido> list = pedidoService.findPage(page, linesPerPage, orderBy, direction);
+        return ResponseEntity.ok().body(list);
     }
 
 }
--- src/main/java/com/moslite/orderbackend/services/PedidoService.java
+++ src/main/java/com/moslite/orderbackend/services/PedidoService.java
@@ -1,14 +1,17 @@
 package com.moslite.orderbackend.services;
 
-import com.moslite.orderbackend.domain.ItemPedido;
-import com.moslite.orderbackend.domain.PagamentoComBoleto;
-import com.moslite.orderbackend.domain.Pedido;
+import com.moslite.orderbackend.domain.*;
 import com.moslite.orderbackend.domain.enums.EstadoPagamento;
 import com.moslite.orderbackend.repositories.ItemPedidoRepository;
 import com.moslite.orderbackend.repositories.PagamentoRepository;
 import com.moslite.orderbackend.repositories.PedidoRepository;
+import com.moslite.orderbackend.security.UserSecurity;
+import com.moslite.orderbackend.services.exceptions.AuthorizationException;
 import com.moslite.orderbackend.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -70,6 +73,16 @@ public class PedidoService {
         itemPedidoRepository.saveAll(obj.getItens());
         emailService.sendOrderConfirmationHtmlEmail(obj);
         return obj;
+    }
+
+    public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        UserSecurity user = UserService.authenticated();
+        if (user == null) {
+            throw new AuthorizationException("Acesso negado");
+        }
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Cliente cliente = clienteService.find(user.getId());
+        return pedidoRepository.findByCliente(cliente, pageRequest);
     }
 
 }
